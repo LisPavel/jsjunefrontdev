369622b35ecc80ccbcce437311790c6bccc78226
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-02-24 11:39:53 +0300

    NEW: rollup style
    REDO: reorganize code
    NEW: extract css styles ti single file in dist

--- module1/lesson18/task1/rollup.config.js
+++ module1/lesson18/task1/rollup.config.js
@@ -1,10 +1,21 @@
+import styles from "rollup-plugin-styles";
+
+const targetDir = "dist";
+const srcDir = "src";
+
 export default {
-  input: "index.js",
+  input: `${srcDir}/index.js`,
   output: {
-    file: "bundle.js",
+    dir: targetDir,
     format: "cjs",
+    assetFileNames: "[name][extname]", //need this to extract styles without hash name
   },
   watch: {
     exclude: "node_modules/**",
   },
+  plugins: [
+    styles({
+      mode: ["extract", "index.css"],
+    }),
+  ],
 };

86a7d59f96e3d5f31d961689d6d34e071c10787c
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-02-24 13:01:54 +0300

    NEW: task done

--- module1/lesson18/task1/dist/index.js
+++ module1/lesson18/task1/dist/index.js
@@ -15,4 +15,7 @@ const createImage = () => {
 };
 
 console.log("Hello world!");
+const header = document.createElement("h1");
+header.textContent = "I Love JavaScript";
+document.body.append(header);
 createImage();
--- module1/lesson18/task1/src/index.js
+++ module1/lesson18/task1/src/index.js
@@ -2,4 +2,10 @@ import { createImage } from "./components/js-image";
 import "./index.css";
 
 console.log("Hello world!");
+
+const header = document.createElement("h1");
+header.textContent = "I Love JavaScript";
+
+document.body.append(header);
+
 createImage();

38d198d222922abac489f7e6190a834d003f2c1f
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-02-24 11:44:10 +0300

    NEW: copy html to dist

--- module1/lesson18/task1/rollup.config.js
+++ module1/lesson18/task1/rollup.config.js
@@ -1,21 +1,29 @@
 import styles from "rollup-plugin-styles";
+import copy from "rollup-plugin-copy";
 
 const targetDir = "dist";
 const srcDir = "src";
 
 export default {
   input: `${srcDir}/index.js`,
+
   output: {
     dir: targetDir,
     format: "cjs",
     assetFileNames: "[name][extname]", //need this to extract styles without hash name
   },
+
   watch: {
     exclude: "node_modules/**",
   },
+
   plugins: [
     styles({
       mode: ["extract", "index.css"],
+    }),
+
+    copy({
+      targets: [{ src: `${srcDir}/index.html`, dest: `${targetDir}` }],
     }),
   ],
 };

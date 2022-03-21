8f7f7f4d91eb9032e068b657343c6467a3d29c7a
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-03-21 18:52:01 +0300

    work with array state

--- module2/lessons-app/src/components/counter.jsx
+++ module2/lessons-app/src/components/counter.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 const Counter = () => {
   // let count = 2;
   const [count, setCount] = useState(0);
+  const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
 
   const formatCount = () => {
     return count === 0 ? "Empty" : count;
@@ -21,9 +22,23 @@ const Counter = () => {
   const handleDecrement = (ev) => {
     setCount((prevState) => (prevState > 0 ? prevState - 1 : prevState));
   };
+  const handleTagChange = (ev) => {
+    setTags(["tag4", "tag5"]);
+  };
 
   return (
     <>
+      <ul>
+        {tags.map((tag) => (
+          <li
+            key={tag}
+            className="btn btn-primary btn-sm m-2"
+            onClick={handleTagChange}
+          >
+            {tag}
+          </li>
+        ))}
+      </ul>
       <span className={getBadgeClasses()}>{formatCount()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +

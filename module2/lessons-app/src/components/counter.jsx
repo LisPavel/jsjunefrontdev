f28b72c6717114383ca9e05da676c2097cf5d989
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-03-21 18:29:18 +0300

    handle increment

--- module2/lessons-app/src/components/counter.jsx
+++ module2/lessons-app/src/components/counter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Counter = () => {
-  const count = 2;
+  let count = 2;
   const formatCount = () => {
     return count === 0 ? "Empty" : count;
   };
@@ -12,10 +12,17 @@ const Counter = () => {
     return classes;
   };
 
+  const handleIncrement = (ev) => {
+    count += 1;
+    console.log(count);
+  };
+
   return (
     <>
       <span className={getBadgeClasses()}>{formatCount()}</span>
-      <button className="btn btn-primary btn-sm m-2">+</button>
+      <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
+        +
+      </button>
     </>
   );
 };

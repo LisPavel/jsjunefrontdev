161e818a5a5c31ad51f37f2021ed0fd528689c56
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-03-21 17:39:13 +0300

    react fragment as empty tag

--- module2/lessons-app/src/components/counter.jsx
+++ module2/lessons-app/src/components/counter.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Counter = () => {
   return (
-    <React.Fragment>
+    <>
       <h1>Counter</h1>
       <button className="button">+</button>
-    </React.Fragment>
+    </>
   );
 };
 

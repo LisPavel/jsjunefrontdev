dd88ab363c364b83fde0abc1327d99e04ca4ff8a
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-03-22 10:20:40 +0300

    remove tags on click

--- module2/lessons-app/src/components/counter.jsx
+++ module2/lessons-app/src/components/counter.jsx
@@ -22,8 +22,10 @@ const Counter = () => {
   const handleDecrement = (ev) => {
     setCount((prevState) => (prevState > 0 ? prevState - 1 : prevState));
   };
-  const handleTagChange = (ev) => {
-    setTags(["tag4", "tag5"]);
+  const handleTagChange = (id) => {
+    // setTags(["tag4", "tag5"]);
+    setTags((prevState) => prevState.filter((tag) => tag !== id));
+    console.log(id);
   };
 
   return (
@@ -33,7 +35,7 @@ const Counter = () => {
           <li
             key={tag}
             className="btn btn-primary btn-sm m-2"
-            onClick={handleTagChange}
+            onClick={() => handleTagChange(tag)}
           >
             {tag}
           </li>

3d7b6f8e063da562ce4ce4cdea22c184fccf764f
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-03-28 11:44:11 +0300

    refactor code

--- module2/lessons-app/src/components/counter.jsx
+++ module2/lessons-app/src/components/counter.jsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 const Counter = () => {
   // let count = 2;
   const [count, setCount] = useState(0);
-  const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
 
   const formatCount = () => {
     return count === 0 ? "Empty" : count;
@@ -22,28 +21,9 @@ const Counter = () => {
   const handleDecrement = (ev) => {
     setCount((prevState) => (prevState > 0 ? prevState - 1 : prevState));
   };
-  const handleTagChange = (id) => {
-    // setTags(["tag4", "tag5"]);
-    setTags((prevState) => prevState.filter((tag) => tag !== id));
-    console.log(id);
-  };
-
-  const renderTags = () => {
-    if (tags.length === 0) return "No tags";
-    return tags.map((tag) => (
-      <li
-        key={tag}
-        className="btn btn-primary btn-sm m-2"
-        onClick={() => handleTagChange(tag)}
-      >
-        {tag}
-      </li>
-    ));
-  };
 
   return (
     <>
-      <ul>{renderTags()}</ul>
       <span className={getBadgeClasses()}>{formatCount()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +

a1ba8d3cb9a2d11460f12778aaaa4e133447a644
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-04-26 18:15:36 +0300

    using switch

--- module2/react-router/src/app/App.jsx
+++ module2/react-router/src/app/App.jsx
@@ -1,19 +1,23 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Dashboard from "../components/dashboard";
 import Home from "../components/home";
 import LogIn from "../components/login";
 import NavBar from "../components/navBar";
 import Posts from "../components/posts";
+import Stats from "../components/stats";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <h1>react router</h1>
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/" exact component={Home} />
-      <Route path="/login" component={LogIn} />
-      <Route path="/posts" component={Posts} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/dashboard/stats" component={Stats} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/login" component={LogIn} />
+        <Route path="/posts" component={Posts} />
+      </Switch>
     </div>
   );
 }
